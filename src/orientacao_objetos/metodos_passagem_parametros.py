7fee4d07d29a427432420938aeb2f66cdbb70087
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2025-11-17 09:20:46 -0300

    Feito exercício 03.

--- src/orientacao_objetos/metodos_passagem_parametros.py
+++ src/orientacao_objetos/metodos_passagem_parametros.py
@@ -214,3 +214,97 @@ def criar_pokemons():
      console.print(tabela)
 
 
+class Empresa:
+     def __init__(
+               self,
+               nome_empresa: str,
+               cnpj: str,
+               data_inauguracao: str,
+               faturamento: float,
+               socio: str,
+               tipo_comercio: str = "Vendas",
+               produto_principal: str = "Refrigerantes",
+               pais_origem: str = "Brasil",
+               situacao_cadastro: str = "Ativa",
+               telefone_matriz: str = "(47) 4002-8922"
+               ):
+          self.nome_empresa = nome_empresa
+          self.cnpj = cnpj
+          self.data_inauguracao = data_inauguracao
+          self.faturamento = faturamento
+          self.socio = socio
+          self.tipo_comercio = tipo_comercio
+          self.produto_principal = produto_principal
+          self.pais_origem = pais_origem
+          self.situacao_cadastro = situacao_cadastro
+          self.telefone_matriz = telefone_matriz
+
+
+def criar_empresas():
+     empresas: List[Empresa] = []
+
+     technova = Empresa(
+          nome_empresa= "TechNova Sistemas Ltda",
+          cnpj= "12.345.678/0001-90",
+          data_inauguracao= "21/08/2015",
+          faturamento= 1250000.75,
+          socio= "Marcos Andrade",
+          tipo_comercio= "Tecnologia",
+          produto_principal= "Placa de Vídeo",
+          telefone_matriz= "(11) 4002-8922"
+     )
+
+     greenagro = Empresa(
+          nome_empresa= "GreenAgro Export Ltd.",
+          cnpj= "45.987.123/0001-55",
+          data_inauguracao= "05/11/2001",
+          faturamento= 78000000.30,
+          socio=  "José Carvalho",
+          tipo_comercio= "Agroindústria",
+          produto_principal= "Soja Exportação",
+     )
+
+     nordicola = Empresa(
+          nome_empresa= "NordiCola Ltda",
+          cnpj= "77.222.444/0001-88",
+          data_inauguracao= "30/06/2012",
+          faturamento= 4300000.50,
+          socio= "Erik Johansen",
+     )
+
+     empresas.append(technova)
+     empresas.append(greenagro)
+     empresas.append(nordicola)
+
+     tabela = Table(
+          "Nome",
+          "CNPJ",
+          "Data de Inauguração",
+          "Faturamento",
+          "Sócio",
+          "Tipo de Comércio",
+          "Produto Príncipal",
+          "País de Origem",
+          "Situação",
+          "Telefone Matriz",
+          )
+
+     for i in range(0, len(empresas)):
+          empresa = empresas[i]
+
+          tabela.add_row(
+               empresa.nome_empresa,
+               empresa.cnpj,
+               empresa.data_inauguracao,
+               str(empresa.faturamento),
+               empresa.socio,
+               empresa.tipo_comercio,
+               empresa.produto_principal,
+               empresa.pais_origem,
+               empresa.situacao_cadastro,
+               empresa.telefone_matriz,
+          )
+
+
+     console.print(tabela)
+
